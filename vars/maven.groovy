9712a145ae90c192e8e7af49b3fbe0bf280312cf
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-06 00:31:00 +0530

    adding shared libs

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -7,11 +7,24 @@ def lintchecks(COMPONENT) {
 
 }
 
+def sonarchecks(COMPONENT) {
+
+    sh "echo starting code quality analysis"
+    sh "sonar-scanner -Dsonar.host.url=http://$(SONAR_URL):9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+    sh "echo quality checks done"
+
+}
 
 def call(COMPONENT)    // call is the default functions that is called
 {
         pipeline {
             agent any
+            environment {
+
+                SONAR = credentials('SONAR')
+                SONAR_URL = "172.31.6.105"
+        
+               }
             stages {   // start of stages
 
                 stage("Performing Lint checks") {
@@ -22,6 +35,18 @@ def call(COMPONENT)    // call is the default functions that is called
                         }
                     }
                 }
+
+
+            stage("Performing sonar checks") {
+                    steps {
+                        script {
+
+                            sonarchecks(COMPONENT)    // if the function is in same file, no need to call with filename as prefix
+                        }
+                    }
+                }
+
+
                 stage("Downloading dependencies") {
                     steps {
                         sh "echo mvn clean package"
