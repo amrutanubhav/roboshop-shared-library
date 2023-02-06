688107ed51c581731d3037b1d7f9aa3b136bcc82
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-06 15:15:34 +0530

    adding shared libs

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,6 +14,7 @@ def sonarchecks(COMPONENT) {
     sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
     sh "echo quality checks done"
     sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh"
+    sh "chmod 777 ./sonar-quality-gate.sh"
     sh "sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
 
 }
