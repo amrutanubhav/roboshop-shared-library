cda70a1ae1cd3ced5c4786d4bd312d5cff9d45a5
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-06 16:28:03 +0530

    adding shared libs

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -11,7 +11,7 @@ def lintchecks(COMPONENT) {
 def sonarchecks(COMPONENT) {
 
     sh "echo starting code quality analysis"
-    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
     sh "echo quality checks done"
     sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh"
     sh "chmod 777 ./sonar-quality-gate.sh"
