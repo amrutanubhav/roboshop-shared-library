72db6fc44590ee790cd7c84f057b68d6a8f67720
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-09 18:19:16 +0530

    updating shared libs

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -80,7 +80,7 @@ def call(COMPONENT)    // call is the default functions that is called
                         sh "echo Checking if artifacts exists in repo"
                         script {
                         
-                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip " )
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
                         print UPLOAD_STATUS
 
                         }
