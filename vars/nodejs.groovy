3202fe1c5b1dbaf10b01edf15cfb62fc7ff170a8
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-15 20:53:46 +0530

    updating shared libraries

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,17 +1,17 @@
-def lintchecks(COMPONENT) {
+// def lintchecks() {
 
-    sh "echo installing JSLINT"
-    sh "npm install jslint"
-    sh "ls -ltr node_modules/jslint/bin/"
-    // sh "./node_modules/jslint/bin/jslint.js server.js"
-    sh "echo lint checks done"
+//     sh "echo installing JSLINT"
+//     sh "npm install jslint"
+//     sh "ls -ltr node_modules/jslint/bin/"
+//     // sh "./node_modules/jslint/bin/jslint.js server.js"
+//     sh "echo lint checks done"
 
-}
+// }
 
 def call() {
     node {
             env.APP = "nodejs"
-            lintchecks()
+            common.lintchecks()
             env.ARGS="-Dsonar.sources=."
             common.sonarchecks()
             common.testCases()
