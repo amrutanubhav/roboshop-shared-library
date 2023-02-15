fb0ce2009f23a7d5a7d6bb185297403c0f48db16
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-15 23:25:44 +0530

    updating shared libraries

--- vars/common.groovy
+++ vars/common.groovy
@@ -110,6 +110,18 @@ def artifacts() {
                                 else {
                                         sh "echo artifact is prepared "
                                 }
-            }
+                        }
+                    
+    stage("Uploading Artifacts") {
+
+        withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
+
+            sh "echo exporting binaries to NEXUS"
+            sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file  ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+
+         }
+                    }
+
+
     }
 }
