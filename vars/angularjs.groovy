c174e9cb6eaaa78f404d8bfe8e94a1b0f4541c9b
Amrut Anubhav <anubhavamrut@gmail.com>
2023-02-16 20:17:26 +0530

    updating fronted

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -4,7 +4,7 @@ def call() {
             git branch: 'main', url: "https://github.com/amrutanubhav/${COMPONENT}.git" 
             env.APP == "angularjs"
             common.lintchecks()
-            env.ARGS="-Dsonar.java.binaries=target/"
+            env.ARGS="-Dsonar.sources=."
             common.sonarchecks()
             common.testCases()
             common.artifacts()
