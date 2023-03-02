f2a14b14332d95ee462adb30832dd7fc19a00b51
Amrut Anubhav <anubhavamrut@gmail.com>
2023-03-02 23:31:08 +0530

    updating

--- vars/common.groovy
+++ vars/common.groovy
@@ -24,7 +24,7 @@ stage('Lint Checks') {
             sh "echo installing JSLINT"
             sh "npm install jslint"
             sh "ls -ltr node_modules/jslint/bin/"
-            // sh "./node_modules/jslint/bin/jslint.js server.js"
+            sh "./node_modules/jslint/bin/jslint.js server.js"
             sh "echo lint checks done"
 
         }
