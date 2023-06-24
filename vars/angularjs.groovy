197accd6f5c474886a474eac7edea8058da5f3d0
Amrut Anubhav <anubhavamrut@gmail.com>
2023-06-24 12:28:23 +0530

    updating

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -2,7 +2,7 @@ def call() {
     node {
             sh "rm -rf *"   // removes all existing files in folders
             git branch: 'main', url: "https://github.com/amrutanubhav/${COMPONENT}.git" // pulls the component from Git 
-            env.APP == "angularjs"
+            env.APP = "angularjs"
             common.lintchecks()
             // env.ARGS="-Dsonar.sources=."
             common.sonarchecks()
